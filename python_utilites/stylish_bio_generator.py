a9f3fba0b6f4d252e4a1ba8b3a20d9cbcf558e76
kratik1303 <kratikagrawal55@gmail.com>
2026-04-10 17:24:01 +0530

    Add stylish bio generator and remove self-introduction script from wrong folder

---
+++ python_utilites/stylish_bio_generator.py
@@ -0,0 +1,38 @@
+import textwrap
+
+name = input("What is your name? ").strip()
+profession = input("What is your profession? ").strip()
+passion = input("What is your passion? ").strip()
+emoji = input("What is your favorite emoji? ").strip()
+website = input("What is your website or portfolio link? ").strip()
+
+print ("\n Choose your preferred style for the bio:")
+print("1. Simple lines")
+print("2. Verical flairs")
+print ("3. Emoji Sandwich")
+
+style = input("Enter the number corresponding to your preferred style: ").strip()
+
+def generate_bio(style):
+    if style == "1":
+        return f"{emoji} {name} | {profession}\n 🔥{passion}\n 🔥{website}\n"
+    if style == "2":
+        return f"{emoji} {name}\n {profession}\n 🔥{passion}\n 🔥{website}\n"
+    if style == "3":
+        return f"{emoji*3} {name}\n {profession}\n 🔥{passion}\n 🔥{website}\n {emoji*3} 🔥"
+    
+bio = generate_bio(style)
+print("Your stylish bio : \n")
+print("*"*30)
+print(textwrap.dedent(bio))
+print("*"*30)
+
+save = input("Do u want to save this bio into a txt file ? (y/n)").strip().lower()
+
+if save =='y':
+    filename = f"{name.lower().replace(' ','_')}_bio.txt"
+    with open (filename,'w',encoding = 'utf-8') as f:
+        f.write(bio)
+    print("Bio saved successfully as " + filename)
+    
+    
